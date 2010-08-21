7f4cb566cb2af5bbe726d9e44f4d71c8ef7536b8
Arcticus <Greg.Hrebek@gosyntactix.com>
2010-08-21 12:10:19 -0400

    Added Metrix

--- app/assistants/stage-assistant.js
+++ app/assistants/stage-assistant.js
@@ -1,6 +1,11 @@
+Relego = {};
+
 function StageAssistant() {
 }
 
 StageAssistant.prototype.setup = function() {
+    
+    Relego.Metrix = new Metrix();
+    
     this.controller.pushScene("main");
 };
