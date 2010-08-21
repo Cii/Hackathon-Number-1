101d9d62ffecbb26b8b9266991ad6c7673a90914
Arcticus <Greg.Hrebek@gosyntactix.com>
2010-08-21 13:04:31 -0400

    Created some meat to Main and converted to AppAssistant from StageAssistant.

---
+++ app/assistants/app-assistant.js
@@ -0,0 +1,76 @@
+Relego = {};
+
+//--> Our Stages
+Relego.MainStageName			 	= "Stage";					//--> Our main stage (where we perform, duh!)
+Relego.DashboardStageName		= "Dashboard";			//--> Our dashboard stage (where the bobbleheads go, duh!)
+Relego.Stage					 			= null;
+
+//--> Default Values
+Relego.Database					 		= null;							//--> Our database
+
+
+
+function AppAssistant(appController){
+}
+AppAssistant.prototype.setup = function(){
+	//this.handleLaunch();
+	Relego.Metrix = new Metrix();
+}
+
+//  -------------------------------------------------------
+//  handleLaunch - called by the framework when the application is asked to launch
+AppAssistant.prototype.handleLaunch = function(launchParams){
+	
+	Mojo.Log.info("*** --> handleLaunch Called");
+		
+	var cardStageController = this.controller.getStageController(Relego.MainStageName);
+	var appController = Mojo.Controller.getAppController();
+	
+	Relego.Stage = cardStageController;
+	
+	if (!launchParams)  {
+		//---------------------------------------------------------
+		// FIRST LAUNCH
+		//---------------------------------------------------------
+		if (cardStageController) {
+			// If it exists, just bring it to the front by focusing its window.
+			cardStageController.popScenesTo("main");    
+			cardStageController.activate();
+		}else{
+			// Create a callback function to set up the new main stage once it is done loading. It is passed the new stage controller as the first parameter.
+			var pushMainScene = function(stageController){
+				stageController.pushScene("main");
+			}
+			
+			var stageArguments = {name: GPSFix.MainStageName, lightweight: false};
+			this.controller.createStageWithCallback(stageArguments, pushMainScene.bind(this), "card");
+		}
+	}else{
+		switch (launchParams.action){
+			//---------------------------------------------------------
+			// PING GPS
+			//---------------------------------------------------------
+			case "addtoreaditlater":
+				//--> This should auto save the passed along option
+				//--> What params do we need?
+				break;
+		}
+	}	
+}
+
+// -----------------------------------------
+// handleCommand - called to handle app menu selections
+AppAssistant.prototype.handleCommand = function(event){
+    var stageController = this.controller.getActiveStageController();
+    var currentScene = stageController.activeScene();
+    if(event.type == Mojo.Event.command) {
+        switch(event.command) {
+			case "help":
+				stageController.pushScene("help");
+				break;
+        	case "about":
+				stageController.pushScene("about");
+				break;
+        }
+    }
+}
--- app/assistants/main-assistant.js
+++ app/assistants/main-assistant.js
@@ -2,6 +2,31 @@ function MainAssistant() {}
 
 MainAssistant.prototype = {
     setup: function() {
+    	
+    	// Setup the search filterlist and handlers;
+			this.controller.setupWidget("startSearchField",
+																	{
+																		itemTemplate:"main/relegoRowTemplate",
+																		listTemplate:"main/relegoListTemplate",
+																		filterFunction: this.searchList.bind(this),
+																		renderLimit: 70,
+																		delay: 350
+																	},
+																	this.searchFieldModel = {disabled: false}
+																 );
+    	
+    	// Setup the shipper list, but it's empty
+			this.controller.setupWidget("relegoMainListWgt",
+																	{
+																		itemTemplate:"main/relegoRowTemplate",
+																		listTemplate:"main/relegoListTemplate",
+																		addItemLabel:$L("Add..."),
+																		swipeToDelete:true,
+																		renderLimit: 40,
+																		reorderable:true
+																	},
+																	this.relegoMainListWgtModel = {items: this.articles.list}
+																 );
     },
     activate: function(event) {
     },
