37bdc753b41a59bd38e89903f2bada88a5284959
webOSroundup <roy@webosroundup.com>
2010-08-22 13:36:52 -0400

    Forgot to add splash-assistant.js

---
+++ app/assistants/splash-assistant.js
@@ -0,0 +1,23 @@
+function SplashAssistant() {
+
+}
+
+SplashAssistant.prototype.setup = function() {
+	var prefs = Relego.prefs;
+	if (prefs.username) {
+		API.verifyAccount(prefs, this.authSuccess.bind(this), this.authFail.bind(this));
+	}
+	else {
+		this.controller.stageController.swapScene("auth");
+	}
+
+};
+
+SplashAssistant.prototype.authFail = function(){
+	this.controller.stageController.swapScene("auth");
+};
+
+SplashAssistant.prototype.authSuccess = function(){
+	this.controller.stageController.swapScene("main");
+};
+
